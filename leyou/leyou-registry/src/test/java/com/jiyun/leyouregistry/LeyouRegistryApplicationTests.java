c84307323a550113d632969e9a1ca61580c423ae
ljg <1369159538@qq.com>
2020-09-08 07:32:26 +0800

    day01

---
+++ leyou/leyou-registry/src/test/java/com/jiyun/leyouregistry/LeyouRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyun.leyouregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyouRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
